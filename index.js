02b9daa52cf523b58978aa1a27346270d33f2805
Vishnu V Nair <153673095+Vishnu-V-Nair@users.noreply.github.com>
2025-06-20 06:56:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 const connectDB = require('./config');
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: 'https://jumanji-frontend.onrender.com',
+  credentials: true
+}));
+
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/listings', require('./routes/listings'));
 
